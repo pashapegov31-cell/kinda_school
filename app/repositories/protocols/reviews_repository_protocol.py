13904c8f76a672b26f4d028b80d9e8d5c2d7b39d
Pavel Pegov <pashapegov31@gmail.com>
2026-09-16 20:45:52 +0300

    deleted useless repositories

--- app/repositories/protocols/reviews_repository_protocol.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol
-
-from app.entities.review_entity import ReviewEntity
-
-
-class ReviewsRepository(Protocol):
-    async def create(self, new_review: ReviewEntity) -> ReviewEntity: ...
-    async def get_by_id(self, id: int) -> ReviewEntity | None: ...
