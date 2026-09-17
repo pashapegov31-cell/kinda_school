f0384c6e96fb9495b9c6dc0099b5885848f2f791
Pavel Pegov <pashapegov31@gmail.com>
2026-09-17 17:24:14 +0300

    AuthService

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,12 +1,12 @@
+from app.entities.user_entity import UserEntity
 from app.models.user_model import UserCreate
 from app.repositories.protocols.users_repository_protocol import UsersRepository
-from app.use_cases.registrate_user import registrate_user
+from app.use_cases.registrate_user import RegisterUserUseCase
 
 
 class AuthService:
-    def __init__(self, session: UsersRepository):
-        self.session = session
+    def __init__(self, user_repo: UsersRepository, register_user: RegisterUserUseCase):
+        self._register_user = register_user
 
-    @staticmethod
-    async def register(session, user_data: UserCreate):
-        await registrate_user(session, user_data)
+    async def register_user(self, new_user: UserCreate) -> UserEntity:
+        return await self._register_user.execute(new_user)
