952a698b9ad7b9af20aa2584dd14e26101e9e6f6
Pavel Pegov <pashapegov31@gmail.com>
2026-09-17 17:23:50 +0300

    register use case

--- app/use_cases/registrate_user.py
+++ app/use_cases/registrate_user.py
@@ -1,23 +1,27 @@
 from datetime import datetime, timezone
 
 from app.entities.user_entity import UserEntity
+from app.exceptions.exceptions import UserAlreadyExistsError
 from app.models.user_model import UserCreate
 from app.repositories.inmemory.inmemory_users_repository import InMemoryUsersRepository
+from app.repositories.protocols.users_repository_protocol import UsersRepository
 
-session = InMemoryUsersRepository()
-user_id = -1
+user_repo = InMemoryUsersRepository()
 
 
-async def registrate_user(session, user_data: UserCreate) -> UserEntity:
-    await session.add_email(user_data.email)
-    global user_id
-    user_id += 1
-    new_user = UserEntity(
-        id=user_id,
-        email=user_data.email,
-        name=user_data.name,
-        hashed_password=user_data.password,
-        created_at=datetime.now(tz=timezone.utc),
-    )
-    await session.create(new_user)
-    return new_user
+class RegisterUserUseCase:
+    def __init__(self, user_repo: UsersRepository):
+        self._user_repo = user_repo
+
+    async def execute(self, user_data: UserCreate) -> UserEntity:
+        if await user_repo.exists_email(email=user_data.email):
+            raise UserAlreadyExistsError("Пользователь с данным email уже существует")
+        new_user = UserEntity(
+            id=0,
+            email=user_data.email,
+            name=user_data.name,
+            hashed_password=user_data.password,
+            created_at=datetime.now(tz=timezone.utc),
+        )
+        await self._user_repo.create(new_user)
+        return new_user
