71d0bcaa9957d783ae19faf5f016edf4fd6a4567
Pavel Pegov <pashapegov31@gmail.com>
2026-09-17 19:45:01 +0300

    add UserLogin model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,6 +7,11 @@ class UserCreate(BaseModel):
     password: str
 
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
 class UserResponse(BaseModel):
     email: EmailStr
     name: str
