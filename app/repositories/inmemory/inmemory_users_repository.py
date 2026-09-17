eda4838488cf130126e229f3ced4caf7d99e2232
Pavel Pegov <pashapegov31@gmail.com>
2026-09-17 17:43:28 +0300

    add id making really normally

--- app/repositories/inmemory/inmemory_users_repository.py
+++ app/repositories/inmemory/inmemory_users_repository.py
@@ -2,10 +2,9 @@ from app.entities.user_entity import UserEntity
 
 
 class InMemoryUsersRepository:
-    new_user_id = -1
-
     def __init__(self):
         self._users: dict[int, UserEntity] = {}
+        self._next_id = 0
 
     async def get_by_email(self, email: str) -> UserEntity | None:
         for user in self._users.values():
@@ -23,9 +22,9 @@ class InMemoryUsersRepository:
         return None
 
     async def create(self, new_user: UserEntity) -> UserEntity:
-        InMemoryUsersRepository.new_user_id += 1
-        new_user.id = InMemoryUsersRepository.new_user_id
-        self._users[InMemoryUsersRepository.new_user_id] = new_user
+        new_user.id = self._next_id
+        self._users[self._next_id] = new_user
+        self._next_id += 1
 
         return new_user
 
