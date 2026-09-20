17b4c0d0613bffeeb372a1da73d009d916887f99
Pavel Pegov <pashapegov31@gmail.com>
2026-09-20 10:24:39 +0300

    rework service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,5 +1,4 @@
-from app.entities.user_entity import UserEntity
-from app.models.user_model import UserCreate, UserLogin
+from app.models.user_model import AuthResponse, UserCreate, UserLogin
 from app.repositories.protocols.users_repository_protocol import UsersRepository
 from app.use_cases.login_user import LoginUserUseCase
 from app.use_cases.registrate_user import RegisterUserUseCase
@@ -9,15 +8,17 @@ class AuthService:
     def __init__(
         self,
         user_repo: UsersRepository,
-        register_uc: RegisterUserUseCase,
-        login_uc: LoginUserUseCase,
+        register_uc: RegisterUserUseCase | None,
+        login_uc: LoginUserUseCase | None,
     ):
         self._user_repo = user_repo
         self._register_uc = register_uc
         self._login_uc = login_uc
 
-    async def register(self, new_user: UserCreate) -> UserEntity:
-        return await self._register_uc.execute(new_user)
+    async def register(self, new_user: UserCreate) -> AuthResponse | None:
+        if self._register_uc:
+            return await self._register_uc.execute(new_user)
+        return None
 
-    async def login(self, user: UserLogin) -> UserEntity:
+    async def login(self, user: UserLogin) -> AuthResponse:
         return await self._login_uc.execute(user)
