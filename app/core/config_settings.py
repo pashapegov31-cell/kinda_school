4c604f704fd59367cf4b37c24b2cd5101df560cf
Pavel Pegov <pashapegov31@gmail.com>
2026-09-21 22:20:55 +0300

    add admin settings and access token expires time

--- app/core/config_settings.py
+++ app/core/config_settings.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     TOKEN_SECRET_KEY: str
     ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ADMIN_NAME: str
+    ADMIN_EMAIL: str
+    ADMIN_PASSWORD: str
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False
