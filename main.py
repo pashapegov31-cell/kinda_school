d158a0ebbb98c26b55c6c31c2c856ee5a7b50c0a
Pavel Pegov <pashapegov31@gmail.com>
2026-09-22 11:05:41 +0300

    add change_user_route to app

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.routes.auth_route import auth_router
+from app.routes.change_user_route import change_user_router
 from app.routes.course_route import course_router
 from app.routes.security_route import security_router
 from app.seed_admin import seed_admin
@@ -20,3 +21,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router, prefix="/v1", tags=["Auth"])
 app.include_router(security_router, prefix="/v1", tags=["Me"])
 app.include_router(course_router, prefix="/v1", tags=["Courses"])
+app.include_router(change_user_router, prefix="/v1", tags=["UserChange"])
