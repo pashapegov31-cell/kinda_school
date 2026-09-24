c08fc36c726f0e2ba258727c578d77d53d81febf
Pavel Pegov <pashapegov31@gmail.com>
2026-09-24 22:40:01 +0300

    exception handlers

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
+from app.exceptions.exceptions import (
+    AuthError,
+    ForbiddenError,
+    NotFoundError,
+    TokenError,
+    UserAlreadyExistsError,
+    UserError,
+)
 from app.routes.auth_route import auth_router
 from app.routes.change_user_route import change_user_router
 from app.routes.course_route import course_router
@@ -17,6 +26,37 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.exception_handler(ForbiddenError)
+async def forbidden_handler(request: Request, exc: ForbiddenError):
+    return JSONResponse(status_code=403, content={"detail": str(exc)})
+
+
+@app.exception_handler(AuthError)
+async def auth_handler(request: Request, exc: AuthError):
+    return JSONResponse(status_code=401, content={"detail": str(exc)})
+
+
+@app.exception_handler(TokenError)
+async def token_handler(request: Request, exc: TokenError):
+    return JSONResponse(status_code=401, content={"detail": str(exc)})
+
+
+@app.exception_handler(UserError)
+async def user_handler(request: Request, exc: UserError):
+    return JSONResponse(status_code=404, content={"detail": str(exc)})
+
+
+@app.exception_handler(UserAlreadyExistsError)
+async def user_exists_handler(request: Request, exc: UserAlreadyExistsError):
+    return JSONResponse(status_code=400, content={"detail": str(exc)})
+
+
+@app.exception_handler(NotFoundError)
+async def update_hadler(request: Request, exc: NotFoundError):
+    return JSONResponse(status_code=404, content={"detail": str(exc)})
+
 
 app.include_router(auth_router, prefix="/v1", tags=["Auth"])
 app.include_router(security_router, prefix="/v1", tags=["Me"])
