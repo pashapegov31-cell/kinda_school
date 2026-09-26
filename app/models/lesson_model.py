1cef6b2dddd8ecfb43f93a1554eb0c285c2f01e4
Pavel Pegov <pashapegov31@gmail.com>
2026-09-26 17:28:27 +0300

    LessonListItem model

--- app/models/lesson_model.py
+++ app/models/lesson_model.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, ConfigDict
 
 
 class LessonCreate(BaseModel):
-    course_id: int
     title: str
     content: str
     video_url: str | None
+    after_lesson_id: int
 
 
 class LessonResponse(BaseModel):
@@ -18,3 +18,12 @@ class LessonResponse(BaseModel):
     video_url: str | None
     order: int
     duration_minutes: int
+
+
+class LessonListItem(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    title: str
+    order: int
+    duration_minutes: int | None
